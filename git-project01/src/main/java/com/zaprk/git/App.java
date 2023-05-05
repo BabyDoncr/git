9244e3a3e0f9738124d0aad0b9eae70d3bdb74e4
BabyDoncr <3047727213@qq.com>
2023-05-05 18:15:13 +0800

    master third commit!

--- git-project01/src/main/java/com/zaprk/git/App.java
+++ git-project01/src/main/java/com/zaprk/git/App.java
@@ -9,6 +9,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
         System.out.println("Hello Git!master second commit!");
+        System.out.println("Hello Git!master third commit!");
+
     }
 
 }
