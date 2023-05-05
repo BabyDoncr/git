a04a346ad52b6586f8a4a43bc6e852d1b3a0eb46
BabyDoncr <3047727213@qq.com>
2023-05-05 17:53:21 +0800

    git 初始化配置

---
+++ git-project01/src/main/java/com/zaprk/git/App.java
@@ -0,0 +1,13 @@
+package com.zaprk.git;
+
+/**
+ * @author: LiuKaiLong
+ * @Date: 2023/5/5-17:45
+ */
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
