f52408bc246897e15b4d4f361e732d5c288faedb
BabyDoncr <3047727213@qq.com>
2023-05-05 18:16:07 +0800

    master fours commit!

--- git-project01/src/main/java/com/zaprk/git/App.java
+++ git-project01/src/main/java/com/zaprk/git/App.java
@@ -10,6 +10,7 @@ public class App {
         System.out.println("Hello Git");
         System.out.println("Hello Git!master second commit!");
         System.out.println("Hello Git!master third commit!");
+        System.out.println("Hello Git!master fours commit!");
 
     }
 
