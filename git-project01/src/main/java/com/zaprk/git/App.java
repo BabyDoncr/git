dce215e546945790a1485fc68c32c653bb508512
BabyDoncr <3047727213@qq.com>
2023-05-05 18:12:34 +0800

    master second commit!

--- git-project01/src/main/java/com/zaprk/git/App.java
+++ git-project01/src/main/java/com/zaprk/git/App.java
@@ -8,6 +8,7 @@ package com.zaprk.git;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git!master secondcommit!");
     }
 
 }
